078ae4be043e705ced31123aae21e81ac3f4a7bb
Dona Adhiambo <donaathiambo2017@gmail.com>
2017-10-17 12:17:49 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 jQuery(document).ready(function() {
   jQuery("h1").click(function() {
     alert("This is a header.");
+
+  });
+
+  jQuery("h2").click(function() {
+    alert("This is a header.");
+
+  });
+
+  jQuery("h3").click(function() {
+    alert("This is a header.");
+
+  });
+
+  jQuery("li").click(function() {
+    alert("This is a list.");
+
   });
 
   jQuery("p").click(function() {
@@ -11,4 +27,10 @@ jQuery(document).ready(function() {
     alert("This is an image.");
   });
 });
-alert("hello");
+alert("Hello!! I have a question for you dear:");
+prompt("What is your favorite meal?")
+prompt("fish-stew or meat-stew?")
+prompt("Choose between meat-stew and fish-stew. ")
+alert("Okay, but today i want to show you how to prepare fish-stew,")
+prompt("Are you interested in this deliciuos meal recipe?")
+alert("Thankyou dear, be my guest and let us cook together.")
