7d1eee730109058d0ae379b42938225181361a0c
Dona Adhiambo <donaathiambo2017@gmail.com>
2017-10-17 10:30:56 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,14 @@
-//
-// $function() {
-//   $("h1").click(function() {
-//     alert("This is a header.");
-//   };
-//
-//   $("p").click(function() {
-//     alert("This is a paragraph.");
-//   });
-//
-//   $("img").click(function() {
-//     alert("This is an image.");
-//   });
-// });
-// $function() {
-//   $("h1").click(function() {
-//     alert("This is a header.");
-//   };
-//
-//   $("p").click(function() {
-//     alert("This is a paragraph.");
-//   });
-//
-//   $("img").click(function() {
-//     alert("This is an image.");
-//   });
-// });
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
+});
 alert("hello");
