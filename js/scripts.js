baab070a2f9b07f70c4a2a3fa6000eca90d363ad
Dona Adhiambo <donaathiambo2017@gmail.com>
2017-10-17 09:49:27 +0300

    add changes

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//
+// $function() {
+//   $("h1").click(function() {
+//     alert("This is a header.");
+//   };
+//
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
+//
+//   $("img").click(function() {
+//     alert("This is an image.");
+//   });
+// });
+// $function() {
+//   $("h1").click(function() {
+//     alert("This is a header.");
+//   };
+//
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
+//
+//   $("img").click(function() {
+//     alert("This is an image.");
+//   });
+// });
+alert("hello");
