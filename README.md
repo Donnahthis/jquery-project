7e986ce06c97badabd0fffeedbb662b70b32d038
Dona Adhiambo <donaathiambo2017@gmail.com>
2017-10-18 12:59:03 +0300

    commit changes

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# jquery-project
+# {Application Name}
+
+#### {Brief description of application}, {Date of current version}
+
+#### By **{List of contributors}**
+
+## Description
+
+{This is a project dealing with foods. It provides the reader with different types of food recipes.
+The main aim of the author is to inspire the people who love cooking. Clear steps of making delicious
+meals are also provided within the project.}
+
+## Setup/Installation Requirements
+
+* This project is very inspirational.
+* Follow the instructions where provided in the project
+for positive results.
+* It is easy to understand and the language is simple.
+
+
+## Technologies Used
+
+{The project uses HTML and CSS.}
+
+
+*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+
+Copyright (c) {year} **{List of contributors or company name}**
--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,20 @@
-jQuery(document).ready(function() {
-  jQuery("h1").click(function() {
-    alert("This is a header.");
 
-  });
-
-  jQuery("h2").click(function() {
-    alert("This is a header.");
-
-  });
-
-  jQuery("h3").click(function() {
-    alert("This is a header.");
-
-  });
-
-  jQuery("li").click(function() {
-    alert("This is a list.");
-
-  });
-
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  jQuery("img").click(function() {
-    alert("This is an image.");
+$(function(){
+  $("#btn").click(function(){
+    $("img").toggle();
   });
 });
-alert("Hello!! I have a question for you dear:");
-prompt("What is your favorite meal?")
-prompt("fish-stew or meat-stew?")
-prompt("Choose between meat-stew and fish-stew. ")
-alert("Okay, but today i want to show you how to prepare fish-stew,")
-prompt("Are you interested in this deliciuos meal recipe?")
-alert("Thankyou dear, be my guest and let us cook together.")
+
+$(document).ready(function() {
+        $("button#hello").click(function() {
+          $("ul").prepend("<li>hello!</li>");
+        });
+
+        $("button#goodbye").click(function() {
+          $("ul").prepend("<li>Goodbye!</li>");
+        });
+
+        $("button#stop").click(function() {
+          $("ul").prepend("<li>Stop copying me!</li>");
+        });
+      });
